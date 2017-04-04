92daf9573f6e7d2771283228ebd702ec0e9d276c
Alexey Melezhik <melezhik@gmail.com>
2017-04-04 18:17:40 +0300

    do not import gpg

--- story.bash
+++ story.bash
@@ -1,7 +1,7 @@
 set -e
 
 ruby_version=$(config ruby_version)
-curl -ksSL https://rvm.io/mpapis.asc | gpg2 --import -
+#curl -ksSL https://rvm.io/mpapis.asc | gpg2 --import -
 test -f /etc/profile.d/rvm.sh || curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm reload
